f8e36405c42c5feb18366847723dbd11c322807c
developer <you@example.com>
2023-03-09 17:47:11 +0530

    All Done

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,30 +1,24 @@
 class ExpensesController < ApplicationController
   def index
-    # debugger
-   
     @expense=Expense.new
+# when user is sign out then below condition is execute
     if !user_signed_in?
       redirect_to new_user_session_path
     end
-
+# for fetching data depent on condition
     if @current_user!=nil
-      # @count
       c_id=current_user.id
-
+# by default when user on home page below query is executed
       @expenses=Expense.where("date>=? AND user_id=?",Date.today,c_id)
       if params[:date]!=nil
-        # debugger
         @expenses=Expense.where("date>=? AND user_id=?",params[:date],c_id)
       end
-      # debugger
       if params[:array]!=nil
         if params[:array].size==1
           @expenses=Expense.where("type_of_expenses=? AND date>=? AND user_id=?",params[:array][0],params[:date],c_id)
         elsif  params[:array].size==2
-          # debugger
           @expenses=Expense.where("(type_of_expenses=? OR type_of_expenses=?)AND date>=?AND user_id=?",params[:array][0], params[:array][1],params[:date],c_id)
         elsif params[:array].size==3
-          # debugger
           @expenses=Expense.where("(type_of_expenses=? OR type_of_expenses=? OR  type_of_expenses=?) AND date>=? AND user_id=?",params[:array][0],params[:array][1],params[:array][2],params[:date],c_id)
         elsif params[:array].size==4
           @expenses=Expense.where("(type_of_expenses=? OR type_of_expenses=? OR  type_of_expenses=? OR type_of_expenses=? ) AND date>=? AND user_id=?",params[:array][0],params[:array][1],params[:array][2],params[:array][3],params[:date],c_id)
@@ -32,27 +26,21 @@ class ExpensesController < ApplicationController
           @expenses=Expense.where("(type_of_expenses=? OR type_of_expenses=? OR  type_of_expenses=? OR type_of_expenses=? OR
             type_of_expenses=? ) AND date>=? AND user_id=?",params[:array][0],params[:array][1],params[:array][2],params[:array][3],params[:array][4],params[:date],c_id)
         end
-
       end
     end
-    
-    # debugger
   end
   
   def create
-    # debugger
-    
     if params[:commit]=='Save'
       cid=current_user.id
       expense=Expense.new(param_expense)
       expense.user_id=cid
-      # expense.save!
-      if expense.save!
+      expense.save
+      if expense.save
         redirect_to root_path, notice: "Expenses is successfully added."
       else
-        redirect_to root_path, alert: "Expenses is not added successfully."
+      redirect_to root_path(date: @date,price: @price), notice: "Email or Date can't be blank"
       end
-      # redirect_to root_path
     else
       @date=Date.today
       if params[:date]=="Last week"
@@ -74,7 +62,6 @@ class ExpensesController < ApplicationController
         end
       end
       redirect_to root_path(array: a,date: @date)
- 
     end
   end
 
@@ -84,20 +71,22 @@ class ExpensesController < ApplicationController
   def edit
     @expense=Expense.find(params[:id])
     @date=@expense.date
-
   end
+
   def destroy
     @expense=Expense.find(params[:id])
     @expense.destroy
   end
+
   def update
     @expense=Expense.find(params[:id])
     @expense.update(param_expense)
     redirect_to root_path
-    
   end
+
   private
   def param_expense
     params.require(:expense).permit(:title,:price,:type_of_expenses,:date)
   end
+  
 end
